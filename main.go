b25431c52d88907fc32b9bea34262a923dbcabb2
Phizickz <you@example.com>
2024-10-13 02:36:19 +0200

    start with templ

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"go-todo/pkg/handlers"
+	"github.com/a-h/templ"
+	"go-todo/web/views"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
+	http.Handle("/", templ.Handler(views.root()))
+	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
 
-	http.Handle("/test", http.NotFoundHandler())
-	http.HandleFunc("/test/", handlers.ApiTesting)
+	//http.Handle("/test", http.NotFoundHandler())
+	//http.HandleFunc("/test/", handlers.ApiTesting)
 	
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
